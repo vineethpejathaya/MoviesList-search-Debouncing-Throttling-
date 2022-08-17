2ad835481a3e068b59dab1a0f3abaa4431038e1c
Vineeth Pejathaya <82192818+vineethpejathaya@users.noreply.github.com>
2022-08-17 07:31:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const API_KEY = "api_key=3c5305e1c05f3abcb2085e9e4403fbf4";
 let baseUrl = "https://api.themoviedb.org/3";
-let Api_url = baseUrl + "/movie/latest" + API_KEY;
+let Api_url = baseUrl + "/movie/latest?" + API_KEY;
 let imageUrl = "https://image.tmdb.org/t/p/w500/";
 
+const main = document.getElementById("main");
 getMovies(Api_url);
 
 function getMovies(url) {
@@ -14,22 +15,30 @@ function getMovies(url) {
 }
 
 function showMovies(data) {
+  main.innerHTML = "";
   data.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
-    movieEl.innerHTML = `<img src="" alt="image" />
+    movieEl.innerHTML = `<img src="${imageUrl + movie.poster_path}" alt="${
+      movie.title
+    }" />
     <div class="movie-info">
-      <h3>Movie Title</h3>
-      <span>9.8</span>
+      <h3>${movie.title}</h3>
+      <span class="${getColor(movie.vote_average)}">${movie.vote_average}</span>
     </div>
     <div class="overview">
       <h3>Overview</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam nihil
-        velit libero excepturi dicta ratione voluptas aut, fugit tempore
-        laudantium! Voluptatibus, deserunt aut?
+       ${movie.overview}
       </p>
     </div>`;
+
+    main.appendChild(movieEl);
   });
 }
 
+function getColor(value) {
+  if (value >= 8) return "green";
+  if (5 <= value < 8) return "orange";
+  if (value < 5) return "red";
+}
